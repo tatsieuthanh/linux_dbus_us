a0a774693e829538e99dc516834c39777175cc7a
sieuthanh <tatsieuthanh888@gmail.com>
2024-11-10 08:42:55 +0700

    adjusted makefile

--- src/main.sh
+++ src/main.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Biên dịch chương trình  ===> chmod +x main.sh
+# Accsess file  ===> chmod +x main.sh
 #make
 
-# Thiết lập biến môi trường để tìm thư viện chia sẻ
-export LD_LIBRARY_PATH=..:$LD_LIBRARY_PATH
+# export the library path
+export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
 
-# Thực thi chương trình
+# execute the program
 ./main
